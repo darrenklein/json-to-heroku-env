9f7d5594cedfe4c439f63b0f36938b0633720c23
Darren Klein <darren.a.klein@gmail.com>
2018-03-02 18:28:15 -0500

    Able to get Heroku OAuth token locally

--- bin/json-to-heroku-env.js
+++ bin/json-to-heroku-env.js
@@ -2,6 +2,9 @@
 
 const jsonToHerokuEnv = require('../lib/json-to-heroku-env.js');
 
+const token = process.argv.indexOf('--token') > -1 ? `${process.argv[process.argv.indexOf('--token') + 1]}` : undefined;
 const app = process.argv.indexOf('--app') > -1 ? `${process.argv[process.argv.indexOf('--app') + 1]}` : undefined;
 
-(() => (app ? jsonToHerokuEnv(app) : console.error('Please provide a valid \'--app\' option')))();
+(() => jsonToHerokuEnv(token, app))()
+
+// (() => (app ? jsonToHerokuEnv(app, token) : console.error('Please provide a valid \'--app\' option')))();
