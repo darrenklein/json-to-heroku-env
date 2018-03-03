d6c26fd5942281420ff45dcd48ff150cd4fa09dd
Darren Klein <darren.a.klein@gmail.com>
2018-03-02 21:10:31 -0500

    Pull the token in the bin script

--- bin/json-to-heroku-env.js
+++ bin/json-to-heroku-env.js
@@ -1,10 +1,22 @@
 #! /usr/bin/env node
 
+const exec = require('child_process').exec;
 const jsonToHerokuEnv = require('../lib/json-to-heroku-env.js');
 
 const token = process.argv.indexOf('--token') > -1 ? `${process.argv[process.argv.indexOf('--token') + 1]}` : undefined;
 const app = process.argv.indexOf('--app') > -1 ? `${process.argv[process.argv.indexOf('--app') + 1]}` : undefined;
 
-(() => jsonToHerokuEnv(token, app))()
+exec('heroku auth:token', (error, stdout) => {
+  if (error) {
+    console.error(`exec error: ${error}`);
+    return;
+  }
+
+  return (() => jsonToHerokuEnv(stdout, app))();
+});
+
+
+
+
 
 // (() => (app ? jsonToHerokuEnv(app, token) : console.error('Please provide a valid \'--app\' option')))();
