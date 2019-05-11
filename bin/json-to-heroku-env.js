6ed899cb99f11d10fafe706d2fa2d457891cb0c4
Darren Klein <darren.a.klein@gmail.com>
2019-05-11 16:20:34 -0400

    destructure sep requirement

--- bin/json-to-heroku-env.js
+++ bin/json-to-heroku-env.js
@@ -3,7 +3,9 @@
 /* eslint-disable no-console */
 
 const { exec } = require('child_process');
-const jsonToHerokuEnv = require('../lib/json-to-heroku-env.js');
+const { sep } = require('path');
+
+const jsonToHerokuEnv = require(`..${sep}lib${sep}json-to-heroku-env.js`); // eslint-disable-line import/no-dynamic-require
 
 const app = process.argv.indexOf('--app') > -1 ? `${process.argv[process.argv.indexOf('--app') + 1]}` : undefined;
 const filePath = process.argv.indexOf('--path') > -1 ? `${process.argv[process.argv.indexOf('--path') + 1]}` : undefined;
